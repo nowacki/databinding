f3545123f2132b113322a8a2ee1272b84b04707b
Steffen Nowacki <steffen.nowacki@partmaster.de>
2011-09-23 18:26:44 +0200

    remove deleted files

--- android/de.partmaster.databinding.android/src/de/partmaster/databinding/android/AndroidEventConstants.java
+++
@@ -1,9 +0,0 @@
-package de.partmaster.databinding.android;
-
-public interface AndroidEventConstants {
-
-	int Modify = 1;
-	int FocusOut = 2;
-	int None = 0;
-
-}
--- sample/clock/core/de.partmaster.datadinding.observable.ui.sample.clock.core/src/de/partmaster/databinding/observable/ui/sample/clock/core/ClockMode.java
+++
@@ -1,5 +0,0 @@
-package de.partmaster.databinding.observable.ui.sample.clock.core;
-
-public enum ClockMode {
-	SET, RUN
-}
